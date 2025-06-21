69724a5e5f22e957d1aa95213de7bd86acea865a
NemesisElectron <nemesis_electron@proton.me>
2025-06-21 19:29:19 +0300

    Create SHELL.sh

---
+++ SHELL.sh
@@ -0,0 +1,15 @@
+# Uninstall Google Applications
+pm uninstall --user 0 com.google.android.webview
+
+pm uninstall --user 0 com.google.android.inputmethod.latin
+pm uninstall --user 0 com.google.android.apps.messaging
+pm uninstall --user 0 com.google.android.dialer
+
+pm uninstall --user 0 com.google.android.apps.nbu.files
+pm uninstall --user 0 com.google.android.apps.photos
+pm uninstall --user 0 com.google.android.gm
+pm uninstall --user 0 com.google.android.apps.tachyon
+pm uninstall --user 0 com.google.android.youtube
+
+# Uninstall Android Applications
+pm uninstall --user 0 com.android.chrome
